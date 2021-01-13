2fceddc06a15e19d17a051eee7b46ca39b45877b
Tomás Sacripanti <sacripantitomas@gmail.com>
2021-01-12 22:33:13 -0300

    Removemos el switch de rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
-  Route
 } from "react-router-dom";
 import Header from './components/Header';
 import Homepage from './pages/Homepage';
-// import Blog from './pages/Blog';
 
 
 export class App extends Component {
@@ -14,10 +11,7 @@ export class App extends Component {
     return (
       <Router>
         <Header></Header>
-        <Switch>
-          <Route exact path="/" component={Homepage}></Route>
-          {/* <Route exact path="/blog" component={Blog}></Route> */}
-        </Switch>
+        <Homepage></Homepage>
       </Router>
     )
   }
