597dd7cdadffc38dda9f3ec7a33705d59e2c5343
Tomás Sacripanti <sacripantitomas@gmail.com>
2021-01-19 18:59:59 -0300

    Comenzamos con la maquetación de la sección projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import './styles/Projects.css';
+import notesImg from '../assets/images/mern-notes.png';
 
 export class Projects extends Component {
     render() {
         return (
             <div id="projects" className="projects">
-                
+                <h3 className="projects__title">Some projects i've made</h3>
+                <div className="project project__one">
+                    <div className="project__image">
+                        <img src={notesImg} alt="Notes App (MERN Stack)"/>
+                    </div>
+                    <div className="project__description">
+                        <h4>Notes App</h4>
+                        <p>Aplicación para guardar notas identificadas con su respectivo usuario creada con el stack MERN.</p>
+                        <ul className="project__technologies">
+                            <li>MongoDB</li>
+                            <li>Express.js</li>
+                            <li>React.js</li>
+                            <li>Node.js</li>
+                        </ul>
+                        <div className="project__links">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="project project__two">
+                    
+                </div>
+                <div className="project project__three">
+                    
+                </div>
+                <div className="project project__four">
+                    
+                </div>
             </div>
         )
     }
